88ae0103185c05cae0c96949af5a7ba8651280d4
osukaru16 <osukaru16@gmail.com>
2018-03-08 12:14:09 +0100

    Create Ej1.cs

---
+++ Ej1.cs
@@ -0,0 +1,21 @@
+//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            for(int i = 0; i < 10; i++){
+                Console.WriteLine("Hello, world! "+i);
+            }
+        }
+    }
+}
